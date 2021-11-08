556352204117a68f0ae7d8a3c57ddca98052c6e9
satyadvaya <satyadvaya@gmail.com>
2021-11-08 04:27:00 -0800

    added folder and file structure

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import RestyContainer from '../../containers/RestyContainer';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <RestyContainer />;
 }
